e3cacea0a7213b96d7dd02dbf601f66e46cb92ce
Jack Joseph <dunleavyjack@gmail.com>
2021-01-12 08:41:06 +0900

    Added axios for api request

--- api-picture-search-app/src/components/SearchBar.js
+++ api-picture-search-app/src/components/SearchBar.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
-class SearchBar extends React.Component{
-    onInputchange(event){
-        console.log(event.target.value)
+class SearchBar extends React.Component {
+    state = { term: '' }
+
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term)
     }
-    
+
     render() {
-        return( 
+        return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit}className="ui form">
                     <label>Image Search</label>
-                    <input type="text" onChange={this.onInputchange}/>
+                    <input 
+                        type="text" 
+                        value={this.state.term} 
+                        onChange={(e) => this.setState({ term: e.target.value })} 
+                    />
                 </form>
             </div>
         );
     }
-}
+} 
 
 export default SearchBar;

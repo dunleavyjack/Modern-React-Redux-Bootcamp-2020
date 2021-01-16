e31b1a07b7c71ac78928d0845ab82f90adbc41fa
Jack Joseph <dunleavyjack@gmail.com>
2021-01-16 16:14:43 +0900

    Added dropdown

--- widgets-app/src/components/Dropdown.js
+++ widgets-app/src/components/Dropdown.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', () => {
+            setOpen(false)
+        })
+    }, []);
+
     const renderedOptions = options.map(option => {
+        if (option.value === selected.value) {
+            return null; 
+        }
         return (
-            <div key={option.value}>
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => onSelectedChange(option)}
+            >
                 {option.label}
             </div>
         )
@@ -14,10 +29,10 @@ const Dropdown = ({ options }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">Select A Color</label>
-                <div className="ui selection dropdown visible active">
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
-                    <div className="text">Select Color</div>
-                    <div className="menu visible transition">{renderedOptions}</div>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible' : ''} transition`}>{renderedOptions}</div>
                 </div>
             </div>
         </div>

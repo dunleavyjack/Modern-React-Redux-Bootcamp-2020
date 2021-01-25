df2e4b4badcbaccd5690120badffd0e189e67b95
Jack Joseph <dunleavyjack@gmail.com>
2021-01-26 07:31:16 +0900

    Added userheader and user reducer

--- blog-app/src/reducers/postsReducer.js
+++ blog-app/src/reducers/postsReducer.js
@@ -1,3 +1,10 @@
-export default () => {
-    return 123;
+import { bindActionCreators } from "redux"
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
 }

0319407cf837497bbd56c9eb687317f75c5efcb7
Jack Joseph <dunleavyjack@gmail.com>
2021-01-29 15:12:31 +0900

    Updated auth

--- streaming-app/client/src/components/GoogleAuth.js
+++ streaming-app/client/src/components/GoogleAuth.js
@@ -1,19 +1,40 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
-    componentDidMount(){
+    state = { isSignedIn: null }
+    
+    componentDidMount() {
         console.log(process.env.REACT_APP_CLIENT_ID)
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: process.env.REACT_APP_CLIENT_ID, 
+                clientId: process.env.REACT_APP_CLIENT_ID,
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange);
             })
         })
     }
-    
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+
+
+    renderAuthButton(){
+        if (this.state.isSignedIn === null){
+            return <div>I don't know if we are signed in</div>
+        } else if (this.state.isSignedIn) {
+            return <div>I am signed in</div>
+        } else {
+            return <div>I am not signed in</div>
+        }
+    }
+
     render() {
         return (
-            <div>GoogleAuth</div>
+            <div>{this.renderAuthButton()}</div>
         )
     }
 };
